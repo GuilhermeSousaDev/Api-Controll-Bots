7fc8d25f447055629e8b3e3cecb62cfa25ad8757
Guilherme Augusto <a.guilhermerocha250@gmail.com>
2022-09-12 10:35:32 -0300

    Session Controller and Route

--- src/shared/infra/http/routes/index.routes.ts
+++ src/shared/infra/http/routes/index.routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import browserSearchRouter from '../../../../modules/BrowserBot/infra/http/routes/browserSearch.routes';
 import elementTextSearchRouter from '../../../../modules/BrowserBot/infra/http/routes/element_text_search.routes';
 import incrementUserLevelRouter from '../../../../modules/User/infra/http/routes/increment_user_level.routes';
+import sessionRouter from '../../../../modules/User/infra/http/routes/session.routes';
 import userRouter from '../../../../modules/User/infra/http/routes/user.routes';
 
 const router = Router();
@@ -11,5 +12,7 @@ router.use('/user/level', incrementUserLevelRouter);
 
 router.use('/browser', browserSearchRouter);
 router.use('/browser/element', elementTextSearchRouter);
+
+router.use('/session', sessionRouter);
 
 export default router;
