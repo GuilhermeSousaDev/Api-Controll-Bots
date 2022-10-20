e203f95a4c82ac2c5aa46f485e033e31db56da40
Guilherme Augusto <a.guilhermerocha250@gmail.com>
2022-10-20 17:14:16 -0300

    CreateFuncionalityBotService

--- src/modules/ProgrammableBot/services/CreateFuncionalityBotService.ts
+++ src/modules/ProgrammableBot/services/CreateFuncionalityBotService.ts
@@ -1,5 +1,22 @@
+import { inject, injectable } from "tsyringe";
+import { IPuppeteerProvider } from "../../../shared/infra/container/providers/Puppeteer/models/IPuppeteerProvider";
+
+@injectable()
 export default class CreateFuncionalityBotService {
-    public async execute(funcionality: string) {
-        
+    constructor(
+        @inject('puppeteerProvider')
+        private puppeteerProvider: IPuppeteerProvider,
+    ) {}
+
+    public async execute(funcionality: string): Promise<unknown> {
+        const browser = await this.puppeteerProvider.getPuppeteerAccess();
+
+        const page = await browser.newPage();
+
+        const bodyHandle = await page.$('body');
+
+        const res = await page.evaluate(funcionality, bodyHandle);
+
+        return res;
     }
 }
